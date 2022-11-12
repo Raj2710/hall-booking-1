fff1c20386cb81c8d5185979113931c57f0e12e3
MDMSc <m.s.choudhury96@gmail.com>
2022-11-12 16:26:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { hallRouter } from "./routers.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('Homepage : Hall Bookiing API');
+});
+
+app.use("/api", hallRouter);
+
+app.listen(PORT, console.log("Server running on port " + PORT));
